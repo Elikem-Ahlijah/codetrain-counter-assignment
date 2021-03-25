a6def71e399201c8c94341ce351b146e9a8152d1
Elikem-Ahlijah <agbenyame@gmail.com>
2021-03-25 23:47:51 +0000

    first commit clas and functional component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, Component} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   const [state, setState] = useState({age:20})
+//   const handleClickup = () =>{
+//     let newAge = state.age + 1
+//     setState({age:newAge});
+//   }
+//   const handleClickdown = () =>{
+//     let newAgedd = state.age - 1
+//     setState({age:newAgedd});
+//   }
+//   return (
+//     <div>
+//       <h1>Age is{state.age}</h1>
+//       <button onClick={handleClickup}>Click me up!!</button>
+//       <button onClick={handleClickdown}>Click me down!!</button>
+//     </div>
+//   );
+// }
+
+class App extends Component{
+  constructor(){
+    super()
+    this.state = {age:20}
+  }
+
+  handleClickup = () =>{
+    let newAge = this.state.age + 1;
+    this.setState({age:newAge});
+  }
+
+  handleClickdown = ()=>{
+    let newAgedd = this.state.age - 1
+    this.setState({age:newAgedd});
+  }
+
+
+  render(){
+    return(
+      <div className='App'>
+        <h1>Age is{this.state.age}</h1>
+        <button onClick={this.handleClickup}>Click me up!!</button>
+        <button onClick={this.handleClickdown}>Click me down!!</button>
+     </div>
+    )
+  }
 }
+
 
 export default App;
